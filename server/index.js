d366158b58670803bd432a5c23c6f61403816eea
Ken <ken.girard@gmail.com>
2023-07-15 17:26:00 -0500

    added app.use for cors & express, and status for all other urls

--- server/index.js
+++ server/index.js
@@ -5,8 +5,17 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json("Hello mate!");
+});
+
+// Sends status for all other pages
+app.get("*", (req, res) => {
+  res.sendStatus("404");
 });
 
 app.listen(PORT, ()=> {
