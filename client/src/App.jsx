22a189ea35ef696cd44c542bd1167c6553f5337c
Ken <ken.girard@gmail.com>
2023-07-16 20:25:26 -0500

    Created the first routes: Home & About

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
-
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home/home";
+import About from "./routes/About/about"
 
 function App() {
   return (
     <>
-      Hello 
+      <Router>
+        Header
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        Footer
+      </Router>
     </>
   )
 }
