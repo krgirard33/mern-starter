a7093bf5b116688087b52cc5ab3af39c794f26cf
Ken <ken.girard@gmail.com>
2023-07-16 22:07:36 -0500

    created Book, and added it to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home/home";
-import About from "./routes/About/about"
-import Header from "./components/Header" 
-import Footer from "./components/Footer"
+import About from "./routes/About/about";
+import Book from "./routes/Book/book";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/books" element={<Book />} />
         </Routes>
         <Footer />
       </Router>
